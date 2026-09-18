699b3a7f720ffc96dfea44b18d20ba14e71972f0
3215tanu <3215.tanu@gmail.com>
2026-09-18 19:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,3 +130,4 @@ def verify_proof():
 if __name__ == '__main__':
     print("Starting ZeroTrace Verifier Node on http://127.0.0.1:5000 ...")
     app.run(debug=True, port=5000)
+    #signing off tanu 
